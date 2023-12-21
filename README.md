e18fbed08608e23c0e0f441c6567c3729f0521cc
Murat Ersin <mmuratersin@gmail.com>
2023-12-21 20:17:26 +0300

    browser test

--- README.md
+++ README.md
@@ -1,18 +1,36 @@
 # run-env
 
-Run env is a running envoirenment detector librarry.
+Effortlessly determine the current JavaScript runtime environment with this lightweight module. Seamlessly detects whether your code is running in a browser, Node.js, Deno, or Bun. Simplify cross-platform compatibility checks in your projects.
 
 example:
 
-```ks
-import { IS_BROWSER, BROWSER_NAME_BROWSER_VERSION, IS_NODE, NODE_VERSION } from 'run-env'
+```js
+import {
+  IS_BROWSER,
+  BROWSER_NAME,
+  BROWSER_VERSION,
+  IS_BUN,
+  BUN_VERSION,
+  IS_DENO,
+  DENO_VERSION,
+  IS_NODE,
+  NODE_VERSION
+} from 'run-env'
 
 if (IS_BROWSER) {
-    console.log('Browser name: ', BROWSER_NAME)
-    console.log('Browser version: ', BROWSER_VERSION)
+  console.log('Browser name: ', BROWSER_NAME)
+  console.log('Browser version: ', BROWSER_VERSION)
+}
+
+if (IS_BUN) {
+  console.log(`This code currently runing on bun version: ${BUN_VERSION}`)
+}
+
+if (IS_DENO) {
+  console.log(`This code currently runing on deno version: ${DENO_VERSION}`)
 }
 
 if (IS_NODE) {
-    console.log(`This code currently runing on nodejs version: ${NODE_VERSION}`)
+  console.log(`This code currently runing on nodejs version: ${NODE_VERSION}`)
 }
 ```
--- __test__/browser/browser.test.js
+++ __test__/browser/browser.test.js
@@ -1,75 +1,18 @@
-// import puppeteer from 'puppeteer'
-// import path from 'node:path'
-// import test from 'node:test'
-// import assert from 'node:assert'
-// ;(async () => {
-//   const browser = await puppeteer.launch({
-//     headless: 'new'
-//   })
-//   const page = await browser.newPage()
-
-//   await page.goto('http://localhost:1234')
-
-//   let value = await page.evaluate(
-//     (el) => el.textContent,
-//     await page.$('#is-browser')
-//   )
-//   console.log('------------', value)
-
-//   // const IS_BROWSER = await page.evaluate(
-//   //   () => document.getElementById('is-browser').innerText
-//   // )
-//   // const BROWSER_VERSION = await page.evaluate(
-//   //   () => document.getElementById('browser-version').innerText
-//   // )
-//   // const BROWSER_NAME = await page.evaluate(
-//   //   () => document.getElementById('browser-name').innerText
-//   // )
-
-//   // const IS_BUN = await page.evaluate(
-//   //   () => document.getElementById('is-bun').innerText
-//   // )
-//   // const BUN_VERSION = await page.evaluate(
-//   //   () => document.getElementById('bun-version').innerText
-//   // )
-
-//   // const IS_DENO = await page.evaluate(
-//   //   () => document.getElementById('is-deno').innerText
-//   // )
-//   // const DENO_VERSION = await page.evaluate(
-//   //   () => document.getElementById('deno-version').innerText
-//   // )
-
-//   // const IS_NODE = await page.evaluate(
-//   //   () => document.getElementById('is-node').innerText
-//   // )
-//   // const NODE_VERSION = await page.evaluate(
-//   //   () => document.getElementById('node-version').innerText
-//   // )
-
-//   // test('run-env test on nodejs...', () => {
-//   //   assert.strictEqual(IS_BROWSER, 'true')
-//   //   assert.notStrictEqual(BROWSER_VERSION, 'undefined')
-//   //   assert.notStrictEqual(BROWSER_NAME, 'undefined')
-
-//   //   assert.strictEqual(IS_BUN, 'false')
-//   //   assert.strictEqual(BUN_VERSION, 'undefined')
-
-//   //   assert.strictEqual(IS_DENO, 'false')
-//   //   assert.strictEqual(DENO_VERSION, 'undefined')
-
-//   //   assert.strictEqual(IS_NODE, 'false')
-//   //   assert.strictEqual(NODE_VERSION, 'undefined')
-//   // })
-
-//   // await browser.close()
-// })()
-
 import { test, expect } from '@playwright/test'
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:1234/')
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Document/)
+  await expect(page.getByTestId('is-browser')).toHaveText('true')
+  await expect(page.getByTestId('browser-name')).not.toHaveText('undefined')
+  await expect(page.getByTestId('browser-version')).not.toHaveText('undefined')
+
+  await expect(page.getByTestId('is-bun')).toHaveText('false')
+  await expect(page.getByTestId('bun-version')).toHaveText('undefined')
+
+  await expect(page.getByTestId('is-deno')).toHaveText('false')
+  await expect(page.getByTestId('deno-version')).toHaveText('undefined')
+
+  await expect(page.getByTestId('is-node')).toHaveText('false')
+  await expect(page.getByTestId('node-version')).toHaveText('undefined')
 })
